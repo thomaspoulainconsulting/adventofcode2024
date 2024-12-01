2f1c94ab7af52e7497c18a0b64bf99a74cf04b3f
Thomas Poulain <thomas.poulain.partner@decathlon.com>
2024-12-01 21:03:29 +0100

    fix: code review

--- src/main/kotlin/days/Day1.kt
+++ src/main/kotlin/days/Day1.kt
@@ -6,7 +6,11 @@ class Day1 : Day(1, "Historian Hysteria") {
 
     override fun solvePart1(input: List<String>): String {
         val (firstCol, lastCol) = input
-            .map { it.split("   ").let { (a, b) -> a.toInt() to b.toInt() } }
+            .map {
+                it.split("   ").let { (a, b) ->
+                    a.toInt() to b.toInt()
+                }
+            }
             .unzip()
             .let { (first, second) ->
                 first.sorted() to second.sorted()
@@ -26,9 +30,9 @@ class Day1 : Day(1, "Historian Hysteria") {
                 first.sorted() to second.sorted()
             }
 
-        return input.indices
-            .sumOf { index ->
-                firstCol[index] * lastCol.count { it == firstCol[index] }
+        return firstCol
+            .sumOf { element ->
+                element * lastCol.count { it == element }
             }
             .toString()
     }
